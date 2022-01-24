72940169c5fcc81b7a63eac4730ab0dc7d46cc91
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-24 09:07:47 +0100

    superglobales

--- index.php
+++ index.php
@@ -3,7 +3,25 @@
 /**
  * A l'aide de la documentation officielle de PHP.
  * 1. Retrouvez la page traitant des superglobales pour $_SERVER
- * 2. Récupérez un maximum de valeurs de ce tableau et affichez les à l'aide d'un simple echo ( il n'est pas interdit d'être intelligent, c'est un tableau associatif... )
+ * 2. Récupérez un maximum de valeurs de ce tableau et affichez les à l'aide d'un simple echo
+ ( il n'est pas interdit d'être intelligent, c'est un tableau associatif... )
  * 3. Uploadez ce projet sur votre serveur et testez !
  */
 // TODO Votre code ici
+$server = [
+    $_SERVER['PHP_SELF'],
+    $_SERVER['SERVER_NAME'],
+    $_SERVER['SERVER_ADDR'],
+    $_SERVER['REMOTE_ADDR'],
+    $_SERVER['HTTPS'],
+    $_SERVER['REQUEST_TIME'],
+    ];
+
+foreach ($server as $value) {
+    if (isset($_SERVER[$value])) {
+        echo $value .$_SERVER[$value];
+    }
+    else {
+        echo $value;
+    }
+}
